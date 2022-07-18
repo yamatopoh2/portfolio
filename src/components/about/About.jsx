00b7d526c6a7b7bd09e91a748cc0f7d5093d38ef
yamatopoh2 <109258422+yamatopoh2@users.noreply.github.com>
2022-07-18 15:01:38 +0800

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,7 +7,7 @@ import { VscFolderLibrary } from "react-icons/vsc"
 const About = () => {
   return (
     <section id="about">
-      <h5>Get to Know!</h5>
+      <h5>Get to Know</h5>
       <h2>About Me</h2>
       <div className="container about__container">
         <div className="about__me">
